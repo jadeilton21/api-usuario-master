181138df200e04c0a6a4127edc1fbf9ffcb1c3ec
jadei <jadeiltondm@gmail.com>
2024-04-18 16:18:59 -0300

    feat

---
+++ src/main/java/api/usuario/segunda/SegundaApplication.java
@@ -0,0 +1,13 @@
+package api.usuario.segunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaApplication.class, args);
+	}
+
+}
