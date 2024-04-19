e15b3582f89516035e6d54bfb1bb1d85ee319883
jadei <jadeiltondm@gmail.com>
2024-04-19 17:19:05 -0300

    feat: add method ApiInfoBuilder

--- src/main/java/api/usuario/segunda/doc/SwaggerConfig.java
+++ src/main/java/api/usuario/segunda/doc/SwaggerConfig.java
@@ -3,6 +3,7 @@ package api.usuario.segunda.doc;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.Contact;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,5 +17,20 @@ public class SwaggerConfig {
                 "jadeilton",
                 "http://www.jaderasta.com.br",
                 "jade@rasta.com.br");
+    }
+    private ApiInfoBuilder informacoesApi() {
+
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("Title - Rest API");
+        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
+        apiInfoBuilder.license("Licença - jaderasta");
+        apiInfoBuilder.licenseUrl("http://www.jaderasta.com.br");
+        apiInfoBuilder.contact(this.contato());
+
+        return apiInfoBuilder;
+
     }
 }
