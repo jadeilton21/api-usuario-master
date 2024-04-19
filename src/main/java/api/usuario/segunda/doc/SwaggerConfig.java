a6c9637f0fe109086480342fb214dae31782305b
jadei <jadeiltondm@gmail.com>
2024-04-19 17:14:29 -0300

    feat: add anotations Configuration and EnableSwagger2

--- src/main/java/api/usuario/segunda/doc/SwaggerConfig.java
+++ src/main/java/api/usuario/segunda/doc/SwaggerConfig.java
@@ -1,4 +1,11 @@
 package api.usuario.segunda.doc;
 
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 }
