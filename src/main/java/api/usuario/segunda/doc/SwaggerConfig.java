bb9cdf68f7e39e7c39db9e56ba7088d506aa784a
jadei <jadeiltondm@gmail.com>
2024-04-19 17:18:37 -0300

    feat: add method Contact

--- src/main/java/api/usuario/segunda/doc/SwaggerConfig.java
+++ src/main/java/api/usuario/segunda/doc/SwaggerConfig.java
@@ -3,9 +3,18 @@ package api.usuario.segunda.doc;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.Contact;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+
+    private Contact contato() {
+        return new Contact(
+                "jadeilton",
+                "http://www.jaderasta.com.br",
+                "jade@rasta.com.br");
+    }
 }
